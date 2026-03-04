045eab18891808b40e3db22ed9ace134c04f2d10
Jonar-Calco <jonarverbart@live.nl>
2026-03-04 15:45:00 +0100

    verified bugfix and reduced redundant lines of code
    Pickup-time calculator is now functional and accurate.
    
    To be implemented in database interface

--- src/main/java/com/example/util/PickupTimeCalculator.java
+++ src/main/java/com/example/util/PickupTimeCalculator.java
@@ -3,10 +3,8 @@ package com.example.util;
 import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 import com.example.csvrw.CsvImporter;
@@ -22,14 +20,9 @@ public class PickupTimeCalculator {
     }
 
     public ZonedDateTime calculatePickupTime(Order order) {
-
-        TimeFormatHandler timeFormatHandler = new TimeFormatHandler();
-
         Instant utcNow = Instant.now();
         ZonedDateTime utcDateTimeNow = utcNow.atZone(ZoneOffset.UTC);
         DayOfWeek utcDayOfWeekNow = utcDateTimeNow.getDayOfWeek();
-
-        //System.out.println(openingHours.get(utcDayOfWeekNow.toString().charAt(0) + utcDayOfWeekNow.toString().substring(1).toLowerCase()).get("openTill"));
 
         String[] todaysOpeningHourMinute;
         String[] todaysClosingHourMinute = openingHours.get(utcDayOfWeekNow.toString().charAt(0) + utcDayOfWeekNow.toString().substring(1).toLowerCase()).get("openTill").split(":");
@@ -46,7 +39,6 @@ public class PickupTimeCalculator {
         Duration completionDurationLeft = order.getTotalCompletionTime();
 
         Duration openTillCloseToday;
-        
 
         if (completionDurationLeft.minus(nowTillClosingToday).isNegative()) {
             ZonedDateTime pickupTime = utcDateTimeNow.plus(nowTillClosingToday.plus(completionDurationLeft));
@@ -55,8 +47,6 @@ public class PickupTimeCalculator {
             completionDurationLeft = completionDurationLeft.minus(nowTillClosingToday);
             int i = 1;
             while (true) {
-                    // PROBABLY FIXED:There is a bug happening here, maybe when pcikup time is after closing time of order date. More likely: quantity not taken into account.
-                    // BUG: Sunday is now probably interpreted as 24h of opening time instead of being closed the entire day. Can use different notation in CSV if easier.
                     System.out.println(utcDayOfWeekNow.plus(i).toString().charAt(0) + utcDayOfWeekNow.plus(i).toString().substring(1).toLowerCase());
                     todaysOpeningHourMinute = openingHours.get(utcDayOfWeekNow.plus(i).toString().charAt(0) + utcDayOfWeekNow.plus(i).toString().substring(1).toLowerCase()).get("openFrom").split(":");
                     todaysOpeningHour = Integer.parseInt(todaysOpeningHourMinute[0]);
@@ -69,24 +59,20 @@ public class PickupTimeCalculator {
                     utcTodaysClosing = utcDateTimeNow.plusDays(i).withHour(todaysClosingHour).withMinute(todaysClosingMinute);
 
                     openTillCloseToday = Duration.between(utcTodaysOpening, utcTodaysClosing);
+                    System.out.println("Duration open today in seconds: " + openTillCloseToday);
 
                     if (completionDurationLeft.minus(openTillCloseToday).isNegative()) {
                         ZonedDateTime pickupTime = utcDateTimeNow.withHour(todaysOpeningHour).withMinute(todaysOpeningMinute).plusDays(i).plus(completionDurationLeft);
+                        System.out.println("Completion time left in seconds (Add to opening time of pcikup day): " + completionDurationLeft + "\n");
                         return pickupTime;
                     } else {
                         completionDurationLeft = completionDurationLeft.minus(openTillCloseToday);
+                        System.out.println("Completion time left in seconds: " + completionDurationLeft + "\n");
                         i++;
                     }
                 }
-
             }
-                // If durationLeft - durationOpenNextDay = negative then return durationOpenNextDay - durationLeft as ZonedDateTime.
-                // Else: durationLeft = durationLeft - durationOpenNextDay
         }
-
-        //Duration timeLeftOnOrderDay = order.getOrderPlacedTimestamp();
-
-        
-        //totalCompletionTime - timeLeftOnOrderDay = 
+ 
 }
 
