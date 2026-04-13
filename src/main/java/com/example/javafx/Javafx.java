e6006ac12d890c2967f34ad2f6df21f1cba1ec3f
Jonar-Calco <jonarverbart@live.nl>
2026-04-13 14:40:47 +0200

    changed images
    Now using newly created images and paths suitable for build

--- src/main/java/com/example/javafx/Javafx.java
+++ src/main/java/com/example/javafx/Javafx.java
@@ -8,12 +8,18 @@ package com.example.javafx;
 import com.example.constants.Constants;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Javafx extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("PhotoShop Printshop");
+        Image icon = new Image(getClass().getResourceAsStream("/PSIcon128.png"));
+        primaryStage.getIcons().add(icon);
+
         SceneManager.init(primaryStage);
         SceneManager.switchTo("loginScreen.fxml", null);
         if (Constants.LOAD_ITEMS) {
